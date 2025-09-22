c50663c4d44dbde00dbe61ad7f0c75ab17c5450b
ian_git8520 <iandavidnjenga@gmail.com>
2025-09-23 00:52:35 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+
+const add = function(a, b) {
+  return a + b;
+};
+
+
+const multiply = (a, b) => a * b;
+
+
+const square = x => x * x;
+
+
+const sayHi = () => "Hi there!";
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+
+console.log(greet("CodeGrade"));  
+console.log(add(2, 3));           
+console.log(multiply(4, 5));      
+console.log(square(6));           
+console.log(sayHi());             
